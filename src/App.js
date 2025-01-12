cb270578ee4fe09952c0739d09dde41a5e477663
Jacob Adili <jadili@yahoo.com>
2025-01-12 11:52:59 -0600

    adding changes to make gh-pages work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
 import Home from './Home.js';
+import { Router } from 'react-router-dom/cjs/react-router-dom.min.js';
 
 const App = () => {
   return (
-    <div className='container'>
-      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-        <h1>Currency Converter</h1>
-      </nav>
-      <Home />
-      <footer className='text-lg-start bg-body-tertiary text-muted'>
-        <a href='https://github.com/MisterAdili'>See me on GitHub</a>
-      </footer>
-    </div>
+    <Router baename="/currency-converter">
+      <div className='container'>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+          <h1>Currency Converter</h1>
+        </nav>
+        <Home />
+        <footer className='text-lg-start bg-body-tertiary text-muted'>
+          <a href='https://github.com/MisterAdili'>See me on GitHub</a>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
